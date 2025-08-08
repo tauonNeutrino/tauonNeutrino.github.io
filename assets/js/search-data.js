97febdd724919556d922be7ddfddf28c0661a61c
tauonNeutrino <70279496+tauonNeutrino@users.noreply.github.com>
2025-08-08 21:14:00 +0000

    Deploying to gh-pages from @ tauonNeutrino/tauonNeutrino.github.io@d659068b1361251a25a2a83b4e8d2473d2d29804 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -451,9 +451,9 @@ ninja.data = [{
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
           description: "",
-          section: "News",},{id: "projects-project-1",
-          title: 'project 1',
-          description: "with background image",
+          section: "News",},{id: "projects-image-metrology",
+          title: 'Image Metrology',
+          description: "An internship in Dortmund, DE led me to developing my skills in image metrology and software engineering. My most intensive project in coding, the support I received from my supervisors helped me increase the resolution and detection capabilities of their software near tenfold.",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_project/";
             },},{id: "projects-project-2",
