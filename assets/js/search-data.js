2bac0181bda366796bdf0994175f088fe137289c
tauonNeutrino <70279496+tauonNeutrino@users.noreply.github.com>
2025-08-10 21:47:34 +0000

    Deploying to gh-pages from @ tauonNeutrino/tauonNeutrino.github.io@bbad3a36586ec32bc8cf77681d45305cbed36a4b 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -456,9 +456,9 @@ ninja.data = [{
           description: "An internship in Dortmund, DE led me to developing my skills in image metrology and software engineering. My most intensive project in coding, the support I received from my supervisors helped me increase the resolution and detection capabilities of their software near tenfold.",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_project/";
-            },},{id: "projects-project-2",
-          title: 'project 2',
-          description: "a project with a background image and giscus comments",
+            },},{id: "projects-quantum-magic",
+          title: 'Quantum Magic',
+          description: "Focused on discovering the relation between non-stabilizerness (magic) and barren plateaus.",
           section: "Projects",handler: () => {
               window.location.href = "/projects/2_project/";
             },},{id: "projects-project-3-with-very-long-name",
