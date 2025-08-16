a673cae36e72ea2d843d5be8dab1d61ddea6c134
tauonNeutrino <70279496+tauonNeutrino@users.noreply.github.com>
2025-08-16 21:33:04 +0000

    Deploying to gh-pages from @ tauonNeutrino/tauonNeutrino.github.io@3a1edb464893f65579048ef21b7f8c15ae0e034d 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -466,11 +466,6 @@ ninja.data = [{
           description: "A small venture into the embedding statistics for a quantum annealer.",
           section: "Projects",handler: () => {
               window.location.href = "/projects/3_project/";
-            },},{id: "projects-project-6",
-          title: 'project 6',
-          description: "a project with no image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/6_project/";
             },},{id: "projects-project-7",
           title: 'project 7',
           description: "with background image",
