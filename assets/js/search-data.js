cc5c50e3759e49932ea572a4e3e8071f25b18edb
tauonNeutrino <70279496+tauonNeutrino@users.noreply.github.com>
2025-08-16 21:21:08 +0000

    Deploying to gh-pages from @ tauonNeutrino/tauonNeutrino.github.io@34a585d8f5c4da4c9d859735e0d0bd90503ef371 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -461,9 +461,9 @@ ninja.data = [{
           description: "Focused on discovering the relation between non-stabilizerness (magic) and barren plateaus.",
           section: "Projects",handler: () => {
               window.location.href = "/projects/2_project/";
-            },},{id: "projects-project-3-with-very-long-name",
-          title: 'project 3 with very long name',
-          description: "a project that redirects to another website",
+            },},{id: "projects-embedding-with-dwave",
+          title: 'Embedding with DWave',
+          description: "A small venture into the embedding statistics for a quantum annealer.",
           section: "Projects",handler: () => {
               window.location.href = "/projects/3_project/";
             },},{id: "projects-project-4",
