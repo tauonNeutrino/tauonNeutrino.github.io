2b59db6d3a3b2ae1badcd8f727a9679092944ff6
tauonNeutrino <70279496+tauonNeutrino@users.noreply.github.com>
2025-08-17 19:08:02 +0000

    Deploying to gh-pages from @ tauonNeutrino/tauonNeutrino.github.io@3975d3613f2113cf79d136c6c5f4fbc89b905334 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -53,7 +53,7 @@ ninja.data = [{
           },
         },{id: "nav-people",
           title: "people",
-          description: "members of the lab or group",
+          description: "Myself and my affiliates - I am always finding more!",
           section: "Navigation",
           handler: () => {
             window.location.href = "/people/";
